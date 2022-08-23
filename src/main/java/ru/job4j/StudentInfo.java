817694b22663a7f7d897b1cb71b8fbef09043fc5
Никита <nightwaymoon@gmail.com>
2022-08-24 02:51:25 +0300

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Nikita Ovechkin");
         System.out.println("29.10.1998");
     }
