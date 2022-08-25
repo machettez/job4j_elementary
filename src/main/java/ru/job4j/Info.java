1d72a6edd6c65b1d9b4706d666937b95ca11cd1f
Никита <nightwaymoon@gmail.com>
2022-08-25 08:55:26 +0300

    2. Переменные. [#172635 #336201]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,9 +1,15 @@
 package ru.job4j;
 
-public class Main {
+public class Info {
     public static void main(String[] args) {
-        int age = 18;
-        float length = 78.0332F;
-        boolean baby = true;
+        int age = 23;
+        char gender = 'М';
+        double height = 1.9;
+        int brick = 25000;
+        int bacteria = 10000000;
+        int moon = 384399;
+        boolean job = true;
+        int file = 1048576;
+        System.out.println(bacteria);
     }
 }
