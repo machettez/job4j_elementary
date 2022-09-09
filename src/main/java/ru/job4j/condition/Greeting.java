e157441644f7dcc8014c0bcf1e9efaddd1f5aff2
Никита <nightwaymoon@gmail.com>
2022-09-09 17:01:27 +0300

    9.1. String [#237407 #336841] // правки

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -5,8 +5,9 @@ public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
         System.out.println(idea);
+        idea += " But I am a newbie. ";
         int year = 2022;
-        idea += " But I am a newbie. " + year;
+        idea += year;
         System.out.println(idea);
     }
 
