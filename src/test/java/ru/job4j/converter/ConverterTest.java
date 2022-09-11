60c4391f0a9bae2e88c60be87b87c7d2ccb95959
Никита <nightwaymoon@gmail.com>
2022-09-12 02:10:45 +0300

    1. Модульные тесты. [#159435] // правки

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -15,7 +15,7 @@ public class ConverterTest {
     }
 
     @Test
-    public void whenConvert200RblThen3dot3333Dollar() {
+    public void whenConvert180RblThen3Dollar() {
         float in = 180;
         float expected = 3;
         float out = Converter.rubleToDollar(in);
