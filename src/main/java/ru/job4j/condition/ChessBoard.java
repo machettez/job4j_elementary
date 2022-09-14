edf911f83127f969ddc7ad997bbb708655c7e4db
Никита <nightwaymoon@gmail.com>
2022-09-14 15:43:11 +0300

    4.3.6. Math.abs [#373616] // тесты

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -10,7 +10,7 @@ public class ChessBoard {
                 rsl = Math.abs(x1 - x2);
             }
         }
-        return rsl;
+        return rsl; //test
     }
 
     private static boolean isValid(int coordinate) {
