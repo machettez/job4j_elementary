65eaaa795a4ae80d0c939dab988f0f12f7c304ef
Никита <nightwaymoon@gmail.com>
2022-09-15 19:17:11 +0300

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public static int year(double amount, double salary, double percent) {
+        int year = 0;
+        while (amount > 0) {
+            percent /= 100;
+            amount = amount + amount * percent - salary;
+            year += 1;
+        }
+        return year;
+    }
+
+}
