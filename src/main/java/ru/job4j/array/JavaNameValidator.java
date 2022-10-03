e9d1858b130bc2184c15d56279f4ef1643e2bdae
Никита <nightwaymoon@gmail.com>
2022-10-03 22:32:01 +0300

    6.9. String - это массив [#504884]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -17,15 +17,15 @@ public class JavaNameValidator {
     }
 
     public static boolean isSpecialSymbol(int code) {
-        return (code == 36 || code == 95);
+        return code == 36 || code == 95;
     }
 
     public static boolean isUpperLatinLetter(int code) {
-        return (code >= 65 && code <= 90);
+        return code >= 65 && code <= 90;
     }
 
     public static boolean isLowerLatinLetter(int code) {
-        return (code >= 97 && code <= 122);
+        return code >= 97 && code <= 122;
     }
 
 }
